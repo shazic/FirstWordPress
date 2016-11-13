a923abb244d532b357399c7c0c30708734002994
Shashank Chattopadhyaya <shashank.chattopadhyaya@gmail.com>
2016-11-13 20:35:20 +0530

    My First Plugin
    First Plugin - created a blank plugin, that can be installed, activated
    and deleted. There is no functionality attached to this plugin yet!

---
+++ wp-content/plugins/Loghorn/loghorn.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Log-Horn
+   Plugin URI: http://localhost
+   Description: a plugin to create awesomeness and spread joy
+   Version: 1.2
+   Author: Mr. Awesome
+   Author URI: http://mrtotallyawesome.com
+   License: GPL2
+   */
+?>
